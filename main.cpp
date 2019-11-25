2564b203acd73450416b8d96eda6bc4f2ae7e392
qbius <mich@lsurowka.email>
2019-11-25 18:40:13 +0100

    init

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+#define 💯 int
+#define 🤔 if
+#define 🤜 (
+#define 🤛 )
+#define 👉 {
+#define 👈 }
+#define 🖐 for
+#define 😀 main
+#define 😷 std
+#define ⬅ =
+#define ♋ ==
+#define ♓ <=
+#define ☪ +=
+#define 💦 <<
+#define ➕ +
+#define 🍺 %
+#define 💢 ;
+#define ⏺ .
+#define 🎶 ::
+#define ❓ ?
+#define ❗ :
+#define 🗨 cout
+#define 📃 string
+#define ✏ to_string
+#define 🕳 empty
+
+💯 😀 🤜 🤛
+👉
+	🖐 🤜 💯 🚗 ⬅ 1 💢 🚗 ♓ 100 💢 ➕ ➕ 🚗 🤛
+	👉
+		😷 🎶 📃 🚚 💢
+
+		🤔 🤜 🚗 🍺 3 ♋ 0 🤛 🚚 ☪ "Fizz" 💢
+		🤔 🤜 🚗 🍺 5 ♋ 0 🤛 🚚 ☪ "Buzz" 💢
+
+		😷 🎶 🗨 💦 🤜 🚚 ⏺ 🕳 🤜 🤛 ❓ 😷 🎶 ✏ 🤜 🚗 🤛 ❗ 🚚 ➕ "!" 🤛 💢
+	👈
+👈
